8e403953a2e42362a4916ddd992f235f1cf1fb45
Dyson Smith <dyson.smith@tooeleschools.org>
2020-09-17 14:13:26 -0600

    new stuff for home

---
+++ High_Scores.py
@@ -0,0 +1,25 @@
+#Dyson Smith Sept 17-2020
+#High Scorse
+name1 = 'Topaz'
+name2 = 'Opal'
+name3 = 'Quartz'#jem stone nameing skeam
+name4 = 'Ruby'
+name6 = 'Onix'
+name7 = 'Emerald'
+name8 = 'Saphire'
+name9 = 'Aquamarien'
+name10 = 'Diamond'
+
+#some scorse may repet becuse of ties
+score1 = 17050
+score2 = 16500
+score3 = 15050
+score4 = 14950
+score5 = 13750
+score6 = 12500#reapeats
+score7 = 11750
+score8 = 10000#reapeats
+
+print('|            High Scores            |')
+print('|::::::::::::::::::::::::::::::::::::::::::|')
+print('|'name1
---
+++ python2.3.py
@@ -0,0 +1,16 @@
+#pythone will let you not loose any data by converting data types
+
+number = 42 #you don't need to state that something is an integer
+number2 = 8.0 #if it has a desimole in the componit or the answer it will come back as a flote
+check = type(number)
+check2 = type(number2) # displays the type of data something is
+
+#answer = number / number2*6+7-(58*9/4)+9-7*8 #divition will always convert to flote
+
+answer = 10%5 #hard coded is not a good practice
+#if something is sepose to change make it variables
+
+print("pie"+str(number)) #multiplying a string repeats the sting and adding strings combinds them
+#casting is how str converts to string int to integer float to flote and bool to boolion
+print(answer)
+print(type(answer))
