24e650e58a33a881e731ebfa020768588b440a3f
DistortedTopaz152 <71038866+DistortedTopaz152@users.noreply.github.com>
2021-03-18 14:08:57 -0600

    Delete Triangle_Code.py

--- Triangle_Code.py
+++
@@ -1,9 +0,0 @@
-#Dyson Smith Sept 17-2020
-#Math Lab - Triangles
-
-base = 10
-height =5.0
-area = .5*base*height #finding the area of the triangle useing variables
-
-type(area)#displaying type of data as well as the answer to the "area" equation
-print(area)
